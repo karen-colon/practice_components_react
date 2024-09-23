c6ae718f53d4d2a3884a337acdb31516fa62fb0b
Karen Colon <kscaraballo@hotmail.com>
2024-09-23 02:15:15 -0500

    My fist component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,23 @@
+
+
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <div>MyFirstComponent</div>
+    <h1>Temas de React </h1>
+    <ul>
+      <li>components</li>
+      <li>Eventos</li>
+      <li>Estados de Hooks</li>
+      <li>Props</li>
+      </ul>
+    </>
+  )
+}
+
+
+
+
+
+
